baac83db8e6df3fc3654d11a703777c3a72c9290
Usman <ghaniusman0287@gmail.com>
2025-01-07 21:39:19 +0500

    MongoDb connected With Atlas Service

---
+++ backend/src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+
+        console.log(`\n MongoDB Connected !! DB Host: ${connectionInstance.connection.host}`);
+        // console.log(connectionInstance);
+        
+    } catch (error) {
+        console.error("MongoDB Connection Error: ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
