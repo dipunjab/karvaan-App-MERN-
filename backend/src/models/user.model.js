eee4314f68627610eecbcfafd4cc1f1eb607bd5e
Usman <ghaniusman0287@gmail.com>
2025-01-08 21:18:25 +0500

    Added USer and Video Model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,103 @@
+import mongoose, { Schema } from "mongoose";
+
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, //cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, //cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String,
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+//pre is used for doing something just before saving the document it is mongoose functionality
+//use function in callback not arrow function as arrow dont have 'this' access
+userSchema.pre("save", async function (next) {
+    //if a user changes anything ellse this function will hash the password and save which is not good so we will say only if the password field is modified isModified is given to us
+    if (!this.isModified("password")) return next(); // if not modified return and perform next
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+
+//we are adding a method in which we will put logic to check password
+userSchema.methods.isPasswordCorrect = async function (password) {
+    //bcrypt has functionalty to compare
+   return await  bcrypt.compare(password, this.password)
+}
+
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+
+
+export const User = mongoose.model("User", userSchema)
