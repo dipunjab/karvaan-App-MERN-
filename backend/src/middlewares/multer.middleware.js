8341f253e0f159b05bc895b610d0e6e707d4012a
Usman <ghaniusman0287@gmail.com>
2025-02-03 22:51:26 +0500

    checking-

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,15 +1,34 @@
-import multer from "multer"
+import multer from "multer";
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-})
+const storage = multer.memoryStorage();
 
 export const upload = multer({
-    storage
-})
+    storage,
+    limits: { fileSize: 10 * 1024 * 1024 } 
+});
+
+
+
+
+
+
+
+
+
+
+
+// import multer from "multer"
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, './public/temp')
+//     },
+//     filename: function (req, file, cb) {
+//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+//         cb(null, file.fieldname + '-' + uniqueSuffix)
+//     }
+// })
+
+// export const upload = multer({
+//     storage
+// })
