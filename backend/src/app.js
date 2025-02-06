6dcf70693f4fbe09e7de7d1a822012b59f083b6f
Usman <ghaniusman0287@gmail.com>
2025-02-06 22:06:03 +0500

    increase the limit

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,8 +11,8 @@ app.use(cors({
 }))
 
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: '16kb'}))
+app.use(express.json({limit: "500mb"}))
+app.use(express.urlencoded({extended: true, limit: '500mb'}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
