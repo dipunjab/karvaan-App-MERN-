6416ef9e0fe917c78cc102c36d1fef7be98830dd
Usman <ghaniusman0287@gmail.com>
2025-03-01 10:58:07 +0500

    Playlist deleted Functionality added

--- frontend/src/pages/PlaylistById.jsx
+++ frontend/src/pages/PlaylistById.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
 
 const PlaylistById = () => {
@@ -61,11 +61,11 @@ const PlaylistById = () => {
       );
       const newVideoResponse = await axios.get(`${import.meta.env.VITE_API_BACKEND}/videos/${videoIdToBeAdd}`);
       const newVideo = newVideoResponse.data.data;
-  
+
       // Update state by adding the full video details
       setPlaylistVideos((prevVideos) => [...prevVideos, newVideo]);
 
-      
+
     } catch (error) {
       console.log(error);
     } finally {
@@ -96,12 +96,55 @@ const PlaylistById = () => {
     }
   }
 
+  const [modal, setModal] = useState(false)
+  const confirmdeletePlaylist = () => {
+    setModal(true)
+  }
+
+  const navigate = useNavigate()
+
+  const deletePlaylist = async()=>{
+    try {
+      const response = await axios.delete(
+        `${import.meta.env.VITE_API_BACKEND}/playlist/${playlistId}`
+        ,
+        {
+          headers: {
+            "Authorization": `Bearer ${localStorage.getItem('accessToken')}`,
+          },
+        }
+      );
+      console.log(response);
+      
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false)
+      alert("Playlist Deleted Successfully")
+      navigate("/playlists")
+    }
+  }
+
   return (
     <div>
-      <h1>{playlistData.name}</h1>
-      <p>{playlistData.description}</p>
+      <h1 className='font-bold text-2xl'>Name: <span className='font-semibold text-3xl ml-2'>{playlistData.name}</span></h1>
+      <p className='font-bold text-2xl mb-5'>Descrciption: <span className='font-normal text-xl ml-2'>{playlistData.description}</span></p>
 
       <div>
+        <button className='border border-red-500 p-1 rounded-full bg-red-500 hover:bg-red-400 font-semibold cursor-pointer mb-5' onClick={confirmdeletePlaylist}>Delete Playlist</button>
+        {modal && (
+          <div className='bg-gray-200 p-3 mb-5 rounded-3xl'>
+            <p className='text-xs'>Are you sure you want to delete this Playlist=<span className='font-semibold'>{playlistData.name}</span>?</p>
+
+            <div className='flex gap-10 mt-4'>
+              <button className='p-1 rounded-full bg-white hover:bg-gray-300 font-semibold cursor-pointer mb-5' onClick={()=> setModal(false)}>Cancel</button>
+              <button className='border border-green-500 p-1 rounded-full bg-green-500 hover:bg-green-400 font-semibold cursor-pointer mb-5' onClick={deletePlaylist}>Confirm</button>
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div className='mb-5'>
         <label htmlFor="videoId">VideoId</label>
         <input onChange={(e) => setVideoIdToBeAdd(e.target.value)} type="text" placeholder='video-id-to-add' className='border border-gray-300 ml-2 mr-3 p-1 rounded-4xl' />
         <button className='border border-green-500 p-1 rounded-full bg-green-500 hover:bg-green-400 font-bold cursor-pointer' onClick={addVideoToPlaylist}>Add To List</button>
@@ -110,7 +153,7 @@ const PlaylistById = () => {
       {loading ? (
         <p>Loading...</p>
       ) : playlistVideos.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
           {playlistVideos.map((video) => (
             <div key={video._id}>
               <VideoCard
@@ -121,7 +164,7 @@ const PlaylistById = () => {
                 createdAt={video.createdAt}
                 views={video.views || 0}
               />
-              <button onClick={()=>removeVideofromplaylist(video._id)} className='bg-green-800 font-bold text-white rounded-3xl p-2 cursor-pointer hover:bg-green-500'>Remove</button>
+              <button onClick={() => removeVideofromplaylist(video._id)} className='bg-green-800 font-bold text-white rounded-3xl p-2 cursor-pointer hover:bg-green-500'>Remove</button>
             </div>
           ))}
         </div>
