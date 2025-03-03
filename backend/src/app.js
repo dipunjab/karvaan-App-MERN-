d942d6dfd782d8b5aae3e853fe0c996ad8aa7f70
Usman <ghaniusman0287@gmail.com>
2025-03-03 15:23:40 +0500

    fixing-3

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
     credentials: true
 }))
 
